4d3eac79db7c27f42c1163122dfe34975cf268d8
pkurras <visualbasic@hotmail.de>
2023-07-06 17:54:57 +0200

    Code formatting [*] improved code formatting

--- Plugin.h
+++ Plugin.h
@@ -2,6 +2,24 @@
 // This file added in headers queue
 // File: "Headers.h"
 
-namespace GOTHIC_ENGINE {
-  // Add your code here . . .
+#include <chrono>
+
+namespace GOTHIC_ENGINE
+{
+	class RegenerationTimer
+	{
+		public: RegenerationTimer(void)
+			: StartTimepoint(std::chrono::high_resolution_clock::now())
+		{ }
+		public: void Reset(void)
+		{
+			StartTimepoint = std::chrono::high_resolution_clock::now();
+		}
+		public: double Elapsed(void) const
+		{
+			std::chrono::duration<double> elapsedTime = std::chrono::high_resolution_clock::now() - StartTimepoint;
+			return elapsedTime.count();
+		}
+		private: std::chrono::time_point<std::chrono::high_resolution_clock> StartTimepoint;
+	};
 }
